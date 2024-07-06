c9a7ee5c26717d9b4a725e71e4ecdb4726784485
RakeshKumarMuduli <rakeshmuduli424@gmail.com>
2024-07-06 14:31:48 +0530

    4th commit

--- src/app/Screens/home/page.jsx
+++ src/app/Screens/home/page.jsx
@@ -12,10 +12,10 @@ const App = () => {
   return (
     <div className="flex w-full items-center justify-center h-full p-5">
       <div className="bg-white w-1/2 h-full p-10 rounded-lg text-center">
-        <h1 className="text-4xl font-bold mb-2"> Let's Learn </h1>
-        <h1 className="text-4xl font-bold mb-4">& Earn</h1>
+        <h1 className="text-4xl font-bold mb-2"> Let&apos;s Learn </h1>
+        <h1 className="text-4xl font-bold mb-4">&amp; Earn</h1>
         <p className="text-lg font-medium">Get a chance to win </p>
-        <p className="text-lg mb-8 font-medium">up-to <span className="text-blue-800 text-lg font-bold">Rs. 15,000</span></p>
+        <p className="text-lg mb-8 font-medium">upto <span className="text-blue-800 text-lg font-bold">Rs. 15,000</span></p>
         <button
           className="bg-blue-500 hover:bg-blue-800 text-white py-2 px-4 rounded-lg"
           onClick={handleOpenModal}
